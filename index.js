c2a58c069a9c2d953ac783a25133f7cdf27ac32a
Luis Angel Salcedo Gavidia <seemc9@gmail.com>
2022-05-02 18:57:31 -0500

    feat: github actions

---
+++ index.js
@@ -0,0 +1,3 @@
+import { sum } from "./modules/operations.js";
+
+console.log(sum(4, 5));
---
+++ modules/test/operations.test.js
@@ -0,0 +1,7 @@
+import { sum } from "../operations.js";
+describe("test operations module", () => {
+  it("should solver a sum", () => {
+    const value = sum(1, 4);
+    expect(value).toBe(5);
+  });
+});
